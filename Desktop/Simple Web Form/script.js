79c5f16a9b3ef3c5ec78d8fa8021c2034b305707
bhaskarsqaedevops <bhaskarsqaedevops@gmail.com>
2025-04-02 10:15:00 +0530

    added all files

---
+++ Desktop/Simple Web Form/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('registrationForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const gender = document.getElementById('gender').value;
+        const mobileNumber = document.getElementById('mobileNumber').value;
+        const email = document.getElementById('email').value;
+        
+       
+        document.getElementById('resultFirstName').textContent = firstName;
+        document.getElementById('resultLastName').textContent = lastName;
+        document.getElementById('resultGender').textContent = gender;
+        document.getElementById('resultMobileNumber').textContent = mobileNumber;
+        document.getElementById('resultEmail').textContent = email;
+      
+        document.getElementById('formResults').style.display = 'block';
+    });
+});
